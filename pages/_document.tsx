744cf1f842d5885843dca4f7d3461997605983da
Juan David <juandavid918@gmail.com>
2023-05-23 16:57:20 -0500

    Pantalla home en proceso, sidebar completo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>juanvarela-twitter</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </Head>
+
       <body>
         <Main />
+
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
