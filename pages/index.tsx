1510a70e7afdda8a12f1f1712d8276e237a85218
Juan David <juandavid918@gmail.com>
2023-05-24 20:33:08 -0500

    Interfaz para iniciar sesión y registrarse lista

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 import Header from "@/components/Header";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>juanvarela-twitter</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </Head>
+
       <Header label="Home" />
     </>
   );
